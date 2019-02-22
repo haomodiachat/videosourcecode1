945ef735316940b0ad745f8b083e33e2e7fe4de5
Hao <phunghaoit97@gmail.com>
2019-02-22 18:38:24 +0700

    laravel

---
+++ authen/app/Http/Controllers/AdminController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\AdminModel;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    //hàm khởi tạo của class được chạy nay khi khởi tạo đối tượng
+    //hàm này nó luôn được chạy trước các hàm khác trong class
+    public function __construct()
+    {
+        $this -> middleware('auth:admin') -> only('index');
+    }
+
+    /*
+     * phương thức trả về view khi đăng nhập admin thành công
+     * */
+    public function index() {
+        return view('admin.dashboard');
+    }
+    /*phương thức trả về view dùng để đăng ký tài khoản admin
+     * */
+    public function create() {
+        return view('admin.auth.register');
+    }
+    /*phương thức trả về view dùng để đăng ký tài khoản admin
+     * */
+    public function store(Request $request) {
+        //valideate dữ liệu được gửi từ form đi
+        $this -> validate($request, array(
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required'
+        ));
+        //khởi tạo model để lưu admin mới
+        $adminModel = new AdminModel();
+        $adminModel -> name = $request -> name;
+        $adminModel -> email = $request -> email;
+        $adminModel -> password = bcrypt($request -> password);
+        $adminModel -> save();
+
+        return redirect() -> route('admin.auth.login');
+
+    }
+}
---
+++ authen/app/Http/Controllers/Auth/Admin/LoginController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Auth\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    use AuthenticatesUsers;
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+
+    //phương thức trả về view dùng để đăng nhập cho admin
+    public function login() {
+        return view('admin.auth.login');
+    }
+    /*phương thức này dùng để đăng nhập cho admin
+     * lấy thông tin từ form có method là post
+     * */
+    public function loginAdmin(Request $request) {
+        //validate dữ liệu
+        $this -> validate($request,array(
+            'email' => 'required|email',
+            'password' => 'required|min:6'
+        ));
+        //đăng nhập
+        if(Auth::guard('admin') -> attempt(
+            ['email' => $request -> email, 'password' => $request -> password, $request -> remeber ]
+        )){
+            //nếu đăng nhập thành công thì sẽ chuyển hướng về view dashboard của admin
+            return redirect() -> intended(route('admin.dashboard'));
+        }
+        //nếu đăng nhập thất bại thì quay trở về form đăng nhập
+        //với giá trị của 2 ô input cũ là email và remember
+        return redirect() -> back() -> withInput($request -> only('email', 'remember'));
+    }
+    /*
+     * phương thức này dùng để đăng xuất
+     * */
+    public function logout() {
+        Auth::guard('admin') -> logout();
+        //chuyển hướng về trang login của admin
+        return redirect() -> route('admin.auth.login');
+    }
+}
